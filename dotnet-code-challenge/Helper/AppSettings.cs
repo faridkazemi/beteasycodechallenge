d60e5111ea45491c5b6807e53d934920f99dff1a
farid kazemi <farid.kazemi63@gmail.com>
2018-10-15 23:15:48 +1100

    Fixed the path issue

--- dotnet-code-challenge/Helper/AppSettings.cs
+++ dotnet-code-challenge/Helper/AppSettings.cs
@@ -3,6 +3,6 @@
     public static class AppSettings
     {
         // This should be read from the config file
-        public static string SourcePath => "FeedData";
+        public static string SourcePath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/FeedData";
     }
 }
